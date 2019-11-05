7c536efd3093217c9b8834ef814a938243ef01e5
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-11-05 00:33:23 -0500

    updated more

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,6 +1,7 @@
 package io.zipcoder.polymorphism;
 
 import java.awt.*;
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,6 +17,11 @@ public class MainApplication {
         Scanner sc = new Scanner(System.in);
         System.out.println("\nHow many pets do you have?");
         Integer numberOfPets = sc.nextInt();
+
+//        Console console = new Console();
+//
+//        Integer numberOfPets = console.getIntegerInput("How many pets do you have?");
+
         for (int i = 0; i <= numberOfPets; i++)
         System.out.println(String.format("\nWhat is the name of Pet %d ?", i));
          //this.petNames.add(sc.next());
--- src/main/java/io/zipcoder/polymorphism/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -3,8 +3,13 @@ package io.zipcoder.polymorphism;
 public class Pet {
     String name;
 
+//Constructor
     public Pet (String name) {
         this.name=name;
+    }
+
+    public Pet(){
+
     }
 
     public void setName(String name){
